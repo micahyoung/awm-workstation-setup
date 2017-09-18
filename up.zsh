fa074c7aa74f13b4aa3ab2c6ee4ba2c4c659590c
Example Author <eauthor@pivotal.io>
2017-09-18 15:18:51 -0700

    Initial implementation

---
+++ up.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+source ~/.zshrc
+
+# Fix unhelpful prompts
+if ! [ -d ~/.ssh ]; then
+  mkdir ~/.ssh
+fi
+
+# Force chruby to evaluate
+if ! grep 2.3.4 <(chruby); then
+  ruby-install ruby 2.3.4
+fi
+
+if ! grep "Developer" <(git config --get user.name); then
+  git config --global user.name "Developer"
+fi

21ab1a53ae13b60293544b6217f5ccc401048253
Micah Young <micah.young@gmail.com>
2017-09-19 10:31:56 -0700

    Update up.zsh
    Don't need ruby fix

--- up.zsh
+++ up.zsh
@@ -6,11 +6,6 @@ if ! [ -d ~/.ssh ]; then
   mkdir ~/.ssh
 fi
 
-# Force chruby to evaluate
-if ! grep 2.3.4 <(chruby); then
-  ruby-install ruby 2.3.4
-fi
-
 if ! grep "Developer" <(git config --get user.name); then
   git config --global user.name "Developer"
 fi
